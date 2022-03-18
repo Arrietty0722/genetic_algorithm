23b3016d79cb926f207fe74b4427c2f5a380af6b
wataru-arita <fdyw072222@gmail.com>
2022-03-19 00:04:37 +0900

    tmp

---
+++ atcoder_practice90_1.cpp
@@ -0,0 +1,46 @@
+#include <functional>
+#include <iostream>
+#include <random>
+#include <iterator>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int N;
+    int tmp;
+    vector<int> A;
+    cin >> N;
+    for (int n = 0; n != N; ++n)
+    {
+
+        cin >> tmp;
+        A.push_back(tmp);
+    }
+
+    int res = 0;
+    while (true)
+    {
+        bool exist_odd = false;
+        for (int i = 0; i < N; ++i)
+        {
+            if (A[i] % 2 != 0)
+                exist_odd = true;
+        }
+
+        if (exist_odd)
+            break;
+
+        for (int i = 0; i < N; ++i)
+        {
+            A[i] /= 2;
+        }
+        ++res;
+    }
+
+    cout << res << endl;
+
+    return 0;
+}
