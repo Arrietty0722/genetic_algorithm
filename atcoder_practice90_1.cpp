943af9d1cd793c713c3ffb8571bc10841e062cf1
wataru-arita <fdyw072222@gmail.com>
2022-03-20 19:41:24 +0900

    tmp

--- atcoder_practice90_1.cpp
+++ atcoder_practice90_1.cpp
@@ -7,40 +7,52 @@
 #include <vector>
 using namespace std;
 
+// int main()
+// {
+//     int N;
+//     int tmp;
+//     vector<int> A;
+//     cin >> N;
+//     for (int n = 0; n != N; ++n)
+//     {
+
+//         cin >> tmp;
+//         A.push_back(tmp);
+//     }
+
+//     int res = 0;
+//     while (true)
+//     {
+//         bool exist_odd = false;
+//         for (int i = 0; i < N; ++i)
+//         {
+//             if (A[i] % 2 != 0)
+//                 exist_odd = true;
+//         }
+
+//         if (exist_odd)
+//             break;
+
+//         for (int i = 0; i < N; ++i)
+//         {
+//             A[i] /= 2;
+//         }
+//         ++res;
+//     }
+
+//     cout << res << endl;
+
+//     return 0;
+// }
+
 int main()
 {
-    int N;
-    int tmp;
-    vector<int> A;
-    cin >> N;
-    for (int n = 0; n != N; ++n)
+    int N, A, B;
+    cin >> N >> A >> B;
+    int total = 0;
+    for (int i = 1; i <= N; ++i)
     {
-
-        cin >> tmp;
-        A.push_back(tmp);
+        int sum = findSumOfDigits(i);
     }
-
-    int res = 0;
-    while (true)
-    {
-        bool exist_odd = false;
-        for (int i = 0; i < N; ++i)
-        {
-            if (A[i] % 2 != 0)
-                exist_odd = true;
-        }
-
-        if (exist_odd)
-            break;
-
-        for (int i = 0; i < N; ++i)
-        {
-            A[i] /= 2;
-        }
-        ++res;
-    }
-
-    cout << res << endl;
-
     return 0;
 }
