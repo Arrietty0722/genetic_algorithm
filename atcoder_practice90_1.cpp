8e7dbc15653756e883bf2653defee37fbcca6e43
wataru-arita <fdyw072222@gmail.com>
2022-03-21 23:24:44 +0900

    tmp]

--- atcoder_practice90_1.cpp
+++ atcoder_practice90_1.cpp
@@ -45,30 +45,56 @@ using namespace std;
 //     return 0;
 // }
 
-int findSumOfDigits(int n)
-{
-    int sum = 0;
-    while (n > 0)
-    {
-        sum += n % 10;
-        n /= 10;
-    }
-    return sum;
-}
+// int findSumOfDigits(int n)
+// {
+//     int sum = 0;
+//     while (n > 0)
+//     {
+//         sum += n % 10;
+//         n /= 10;
+//     }
+//     return sum;
+// }
+
+// int main()
+// {
+//     int N, A, B;
+//     cin >> N >> A >> B;
+//     int total = 0;
+//     for (int i = 1; i <= N; ++i)
+//     {
+//         int sum = findSumOfDigits(i);
+//         if (sum >= A && sum <= B)
+//         {
+//             total += i;
+//         }
+//     }
+//     cout << total << endl;
+//     return 0;
+// }
 
 int main()
 {
-    int N, A, B;
-    cin >> N >> A >> B;
-    int total = 0;
-    for (int i = 1; i <= N; ++i)
+    int N;
+    int a[110];
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+        cin >> a[i];
+
+    sort(a, a + N, greater<int>());
+    int Alice = 0;
+    int Bob = 0;
+    for (int i = 0; i < N; ++i)
     {
-        int sum = findSumOfDigits(i);
-        if (sum >= A && sum <= B)
+        if (i % 2 == 0)
         {
-            total += i;
+            Alice += a[i];
         }
+        else
+        {
+            Bob += a[i];
+        }
+        cout << Alice - Bob << endl;
     }
-    cout << total << endl;
     return 0;
 }
